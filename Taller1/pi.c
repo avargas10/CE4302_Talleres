9e7d400d2234db7db2f70b11d810336515dab7d8
avargas10 <afelipe.vargas.r@gmail.com>
2018-08-23 21:34:51 -0600

    PySerial

--- Taller1/pi.c
+++ Taller1/pi.c
@@ -11,15 +11,25 @@ History: Written by Tim Mattson, 11/99.
 #-----------------------------------------------------
 
 Modified by JGG 
-
+Modified by Andres Vargas August 2018
 #--------------------------------------------------------
 
 */
 
 #include <stdio.h>
 #include <omp.h>
+/* 
+Numeros de pasos
+-100000000
+-1000000
+-10000
+-500
+-100
+-10
+-1
 
-static long num_steps = 100000000;
+*/
+static long num_steps = 1;
 double step;
 int main ()
 {
@@ -32,15 +42,18 @@ int main ()
 	/*start timer */
 	start_time = omp_get_wtime();
 
-
+	//Inicio de zona  paralelizable
 	for (i=1;i<= num_steps; i++){
 	x = (i-0.5)*step;
 	sum = sum + 4.0/(1.0+x*x);
 	}
+	//Final Zona paralelizable
 	
 	pi = step * sum;
 	run_time = omp_get_wtime() - start_time;
 	printf("\n pi is %f in %f seconds \n",pi,run_time);
 	
 }	  
+
+
 
